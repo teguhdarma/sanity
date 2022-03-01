7b902560f074340a59695adfe81e6eb131ec533f
teguhdarma <78896400+teguhdarma@users.noreply.github.com>
2022-03-01 10:39:08 +0900

    fini

--- my-airbnc/pages/posts/[id].js
+++ my-airbnc/pages/posts/[id].js
@@ -1,68 +1,68 @@
-import Layout from "../../components/layout";
+// import Layout from "../../components/layout";
 
-export default function Post({ postData }) {
-  return (
-    <Layout>
-      <h1>{ postData.title }</h1>
-      <p>{ postData.body }</p>
-    </Layout>
-  );
-}
+// export default function Post({ postData }) {
+//   return (
+//     <Layout>
+//       <h1>{ postData.title }</h1>
+//       <p>{ postData.body }</p>
+//     </Layout>
+//   );
+// }
 
-async function getAllPostIds(context) {
-    const location = context.query.location
-  const url = "https://travel-advisor.p.rapidapi.com/locations/search?query="+location+"&limit=30&offset=0&units=km&location_id=1&currency=USD&sort=relevance&lang=en_US"
-  const select = await fetch(url, {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "travel-advisor.p.rapidapi.com",
-		"x-rapidapi-key": "08f6102a57msh5971be4c6402572p1e116ejsn5966a16c4d29"
-	}
-}).then(
-    (res) => res.json()
-  );
+// async function getAllPostIds(context) {
+//     const location = context.query.location
+//   const url = "https://travel-advisor.p.rapidapi.com/locations/search?query="+location+"&limit=30&offset=0&units=km&location_id=1&currency=USD&sort=relevance&lang=en_US"
+//   const select = await fetch(url, {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+// 		"x-rapidapi-key": "08f6102a57msh5971be4c6402572p1e116ejsn5966a16c4d29"
+// 	}
+// }).then(
+//     (res) => res.json()
+//   );
 
-  const allPostIds = select.map((post) => {
-    return { params: { id: post.alias } };
-  });
+//   const allPostIds = select.map((post) => {
+//     return { params: { id: post.alias } };
+//   });
 
-  return allPostIds;
-}
+//   return allPostIds;
+// }
 
-export async function getStaticPaths() {
-  const paths = await getAllPostIds();
-  return {
-    paths,
-    fallback: false,
-  };
-}
+// export async function getStaticPaths() {
+//   const paths = await getAllPostIds();
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// }
 
-async function getPostData(id) {
-    const location = context.query.location
-  const url = "https://travel-advisor.p.rapidapi.com/locations/search?query="+location+`&limit=30&offset=0&units=km&location_id=1&currency=USD&sort=relevance&lang=en_US=${id}`
-  const select = await fetch(url, {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "travel-advisor.p.rapidapi.com",
-		"x-rapidapi-key": "08f6102a57msh5971be4c6402572p1e116ejsn5966a16c4d29"
-	}
-}).then(
-    (res) => res.json()
-  );
+// async function getPostData(id) {
+//     const location = context.query.location
+//   const url = "https://travel-advisor.p.rapidapi.com/locations/search?query="+location+`&limit=30&offset=0&units=km&location_id=1&currency=USD&sort=relevance&lang=en_US=${id}`
+//   const select = await fetch(url, {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+// 		"x-rapidapi-key": "08f6102a57msh5971be4c6402572p1e116ejsn5966a16c4d29"
+// 	}
+// }).then(
+//     (res) => res.json()
+//   );
 
-  const postData = select.map((post) => {
-    return { params: { id: post.alias } };
-  });
+//   const postData = select.map((post) => {
+//     return { params: { id: post.alias } };
+//   });
 
-  return postData;
-}
+//   return postData;
+// }
 
 
-export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id);
-  return {
-    props: {
-      postData: postData[0],
-    },
-  };
-}
+// export async function getStaticProps({ params }) {
+//   const postData = await getPostData(params.id);
+//   return {
+//     props: {
+//       postData: postData[0],
+//     },
+//   };
+// }
